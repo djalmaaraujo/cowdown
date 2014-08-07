146ab8c36eacc259b4c1cc86d87e3dcf7a5e4c5e
djalmaaraujo <djalma.araujo@gmail.com>
2014-08-07 12:15:54 -0300

    refactoring youtube code

--- app/assets/javascripts/showdown/youtube.js
+++ app/assets/javascripts/showdown/youtube.js
@@ -1,20 +1,18 @@
 (function(){
+  var youtube = function(converter) {
+    return [
+      { type: 'lang', filter: function(source){
+          return source.replace(/\[youtube\]\(.*\?v=(.*)\)/gi, function(match, videoID) {
+            if (videoID) {
+              return '<iframe width="100%" height="480" src="https://www.youtube.com/embed/' + videoID + '" frameborder="0" allowfullscreen></iframe>';
+            }
+          });
+      }}
+    ];
+  };
 
-    var youtube = function(converter) {
-        return [
-            { type: 'lang', filter: function(source){
-                return source.replace(/\[youtube\]\(.*\?v=(.*)\)/gi, function(match, videoID) {
-                    if (videoID) {
-                      return '<iframe width="100%" height="480" src="https://www.youtube.com/embed/' + videoID + '" frameborder="0" allowfullscreen></iframe>';
-                    }
-                });
-            }}
-        ];
-    };
-
-    // Client-side export
-    if (typeof window !== 'undefined' && window.Showdown && window.Showdown.extensions) { window.Showdown.extensions.youtube = youtube; }
-    // Server-side export
-    if (typeof module !== 'undefined') module.exports = youtube;
-
+  // Client-side export
+  if (typeof window !== 'undefined' && window.Showdown && window.Showdown.extensions) { window.Showdown.extensions.youtube = youtube; }
+  // Server-side export
+  if (typeof module !== 'undefined') module.exports = youtube;
 }());
