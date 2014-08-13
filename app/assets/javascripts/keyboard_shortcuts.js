14b7fe42826d4ef63490fe275166c4b996d5ca91
djalmaaraujo <djalma.araujo@gmail.com>
2014-08-13 00:10:17 -0300

    added support for cmd+] and cmd+[ to use as tabs and shift tabs

--- app/assets/javascripts/keyboard_shortcuts.js
+++ app/assets/javascripts/keyboard_shortcuts.js
@@ -1,6 +1,21 @@
 $(function () {
+  var FAKE_TAB_EVENT = {
+    currentTarget: document.getElementById('cowdown-textarea'),
+    keyCode: 9 // this is the default
+  };
+
   Mousetrap.bind('mod+s', function(e) {
     FileUtils.saveNewOrUpdate();
     e.preventDefault();
+  });
+
+  Mousetrap.bind('mod+]', function(e) {
+    FAKE_TAB_EVENT.shiftKey = false;
+    tabOverride.handlers.keydown(FAKE_TAB_EVENT);
+  });
+
+  Mousetrap.bind('mod+[', function(e) {
+    FAKE_TAB_EVENT.shiftKey = true;
+    tabOverride.handlers.keydown(FAKE_TAB_EVENT);
   });
 });
