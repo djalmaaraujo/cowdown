d988b35023b1a77f5c609822fed0861ce9e9d449
djalmaaraujo <djalma.araujo@gmail.com>
2014-08-16 17:25:26 -0300

    [vimeo] make vimeo image preview

--- app/assets/javascripts/showdown/vimeo.js
+++ app/assets/javascripts/showdown/vimeo.js
@@ -1,18 +1,60 @@
 (function(){
+  var request     = require('request');
+  var Q           = require('q');
+  var VIMEO_CACHE = [];
+  var THUMB_TPL   = '<img src="{{imgSrc}}" alt="{{videoTitle}}" />';
+  var vimeoAPI    = 'http://vimeo.com/api/v2/video/{{id}}.json';
+
+  getThumbTemplate = function (videoID) {
+    var defer = Q.defer();
+
+    if (VIMEO_CACHE[videoID]) {
+      defer.resolve(parseVideoJson(VIMEO_CACHE[videoID]));
+    }
+
+    request({
+      url: vimeoAPI.replace('{{id}}', videoID),
+      method: 'GET',
+      json: true
+    }, function (error, response, body) {
+      if (error) { defer.reject('not-found.png'); }
+
+      VIMEO_CACHE[videoID] = body[0];
+
+      defer.resolve(parseVideoJson(body[0]));
+    });
+
+    return defer.promise;
+  };
+
+  parseVideoJson = function (json) {
+    var tpl = THUMB_TPL;
+
+    tpl = tpl.replace('{{imgSrc}}', json.thumbnail_large);
+    tpl = tpl.replace('{{videoTitle}}', json.title);
+
+    return tpl;
+  };
+
   var vimeo = function(converter) {
     return [
       { type: 'lang', filter: function(source) {
-          return source.replace(/\[vimeo\]\(.*vimeo.com\/(.*)\)/gi, function(match, videoID) {
-            if (videoID) {
-              return '<iframe src="https://player.vimeo.com/video/' + videoID + '" width="100%" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
-            }
-          });
+        return source.replace(/\[vimeo\]\(.*vimeo.com\/(.*)\)/gi, function(match, videoID) {
+          if (VIMEO_CACHE[videoID]) {
+            return parseVideoJson(VIMEO_CACHE[videoID]);
+          }
+          else {
+            getThumbTemplate(videoID).then(function (thumb) {
+              CowdowApp.updatePreview();
+            });
+
+            return '';
+          }
+        });
       }}
     ];
   };
 
-  // Client-side export
   if (typeof window !== 'undefined' && window.Showdown && window.Showdown.extensions) { window.Showdown.extensions.vimeo = vimeo; }
-  // Server-side export
   if (typeof module !== 'undefined') module.exports = vimeo;
 }());
