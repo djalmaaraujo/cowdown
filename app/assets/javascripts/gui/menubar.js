95f8930b7a53a5d1627914280665ea6aed5afeea
djalmaaraujo <djalma.araujo@gmail.com>
2014-08-26 20:44:44 -0300

    fix menubar for new node-webkit version

--- app/assets/javascripts/gui/menubar.js
+++ app/assets/javascripts/gui/menubar.js
@@ -1,15 +1,23 @@
-var gui     = require('nw.gui');
-var fs      = require('fs');
-var win     = gui.Window.get();
+var gui           = require('nw.gui');
+var fs            = require('fs');
+var win           = gui.Window.get();
+var nativeMenuBar = new gui.Menu({ type: "menubar" });
 
-win.menu    = new gui.Menu({ type: 'menubar' });
+nativeMenuBar.createMacBuiltin("CownDown", {
+  hideEdit: true,
+  hideWindow: true
+});
 
-win.menu.insert(new gui.MenuItem({
+win.menu = nativeMenuBar;
+
+var fileMenu = new gui.MenuItem({
   label: 'File',
   submenu: new gui.Menu()
-}), 1);
+});
 
-win.menu.items[0].submenu.append(new gui.MenuItem({
+win.menu.insert(fileMenu, 1);
+
+win.menu.items[1].submenu.append(new gui.MenuItem({
   label: 'New File',
   click: function () {
     gui.Window.open('index.html', {
@@ -18,18 +26,18 @@ win.menu.items[0].submenu.append(new gui.MenuItem({
   }
 }));
 
-win.menu.items[0].submenu.append(new gui.MenuItem({
+win.menu.items[1].submenu.append(new gui.MenuItem({
   type: 'separator'
 }));
 
-win.menu.items[0].submenu.append(new gui.MenuItem({
+win.menu.items[1].submenu.append(new gui.MenuItem({
   label: 'Save',
   click: function () {
     FileUtils.saveNewOrUpdate();
   }
 }));
 
-win.menu.items[0].submenu.append(new gui.MenuItem({
+win.menu.items[1].submenu.append(new gui.MenuItem({
   label: 'Quit',
   click: function () {
     gui.Window.get().close();
