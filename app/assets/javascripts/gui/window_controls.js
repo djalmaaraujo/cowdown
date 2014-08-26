5c3fae6d9750af09c91a3cabf04e894eea61530d
djalmaaraujo <djalma.araujo@gmail.com>
2014-08-26 19:41:54 -0300

    Window control class. Minimize, Fullscreen and Close.

---
+++ app/assets/javascripts/gui/window_controls.js
@@ -0,0 +1,50 @@
+(function () {
+  var gui = require('nw.gui');
+  var win = gui.Window.get();
+
+  var WindowControl = function () {
+    this.listen();
+  };
+
+  WindowControl.prototype.listen = function () {
+    $('.window-control-close').on('click', this.bindClose);
+    $('.window-control-minimize').on('click', this.bindMinimize);
+    $('.window-control-fullscreen').on('click', this.bindFullScreen);
+
+    this.bindExternalLinks();
+  };
+
+  WindowControl.prototype.bindClose = function (e) {
+    e.preventDefault();
+
+    if (window.confirm("Do you really want to leave?")) {
+      win.close();
+    }
+  };
+
+  WindowControl.prototype.bindExternalLinks = function () {
+    $('body').on('click', 'a[href*="http"]', function (e) {
+      e.preventDefault();
+
+      gui.Shell.openExternal(e.target);
+    });
+  };
+
+  WindowControl.prototype.bindMinimize = function (e) {
+    e.preventDefault();
+
+    win.minimize();
+  };
+
+  WindowControl.prototype.bindFullScreen = function (e) {
+    e.preventDefault();
+
+    win.toggleFullscreen();
+  };
+
+  window.WindowControl = WindowControl;
+})();
+
+$(function () {
+  new WindowControl();
+});
