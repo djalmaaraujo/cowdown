420671d6785cc5b084dbc6a45d0828fb06d047e8
djalmaaraujo <djalma.araujo@gmail.com>
2014-08-28 00:10:18 -0300

    refactored application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,22 @@
+(function () {
+  var Application = function () {
+    this.setup();
+  };
+
+  Application.prototype.setup = function () {
+    window.CowdowApp = new Cowdow(Showdown.converter);
+
+    $('textarea')
+      .on('keydown', tabOverride.handlers.keydown)
+      .on('keypress', tabOverride.handlers.keypress);
+
+    CowdowApp.updatePreview();
+    CowdowApp.code.focus();
+  };
+
+  window.Application = Application;
+})();
+
 $(function () {
-  window.CowdowApp = new Cowdow(Showdown.converter);
-
-  $('textarea')
-    .on('keydown', tabOverride.handlers.keydown)
-    .on('keypress', tabOverride.handlers.keypress);
-
-  CowdowApp.updatePreview();
-  CowdowApp.code.focus();
+  new Application();
 });
